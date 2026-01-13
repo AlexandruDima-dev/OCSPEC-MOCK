c43b0fb6f76f0e4af9f244b5341cabaddf7c3348
Alexandru Dima <alexandrucoding08@gmail.com>
2026-01-13 12:12:36 +0000

    I added a backend and a database

---
+++ OCSPEC/server.js
@@ -0,0 +1,68 @@
+const express = require("express")
+const erl = require("express-rate-limit")
+const app = express()
+const Database = require("better-sqlite3")
+const PORT = 8000
+
+//Advanced Middleware
+
+const limiter = erl.rateLimit({
+	windowMs: 15 * 60 * 1000, 
+	limit: 100, 
+	standardHeaders: 'draft-8', 
+	legacyHeaders: false, 
+	ipv6Subnet: 56, 
+})
+
+const logger = (req, res, next)=>{
+    const userhostname = `User Hostname: ${req.hostname}`
+    const userpath = `User Path: ${req.originalUrl}`
+    console.log(userhostname)
+    console.log(userpath)
+    next()
+}
+
+//Middlewear
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(logger);
+app.use(limiter);
+
+//DATA BASE SETUP
+const db = new Database("users.db")
+
+db.prepare(`
+
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    firstName TEXT,
+    lastName TEXT,
+    email TEXT UNIQUE,
+    password TEXT
+    )
+    
+    
+    `).run()
+    console.log("Database is Ready")
+
+
+
+//POST REQUEST
+app.post("/dashboard", (req,res)=>{
+    try{
+        const { firstName, lastName, email, password} = req.body
+        res.status(200).send("Dashboard Data Recived")
+        db.prepare("INSERT INTO users (firstName , lastName, email, password) VALUES (?,?,?,?)").run()
+        console.log(`First name: ${firstName}, Last name: ${lastName}, email:${email}, password: ${password}`)
+    } catch (err){
+        console.log(`Error has been found ${err}`)
+        res.status(500)
+    }
+})
+
+
+//Start the server
+app.listen(PORT, ()=>{
+    console.log("Server is running at: http://localhost:8000")
+
+})
